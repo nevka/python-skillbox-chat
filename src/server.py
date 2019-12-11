e9aa7d462c20470342a83073ed64b73ae003735f
alina <nevkaa@yandex.ru>
2019-12-11 19:45:41 +0300

    DZ

--- src/server.py
+++ src/server.py
@@ -15,6 +15,8 @@ from twisted.protocols.basic import LineOnlyReceiver
 class ServerProtocol(LineOnlyReceiver):
     factory: 'Server'
     login: str = None
+    logins: list = []
+    history_list: list = []
 
     def connectionMade(self):
         # Потенциальный баг для внимательных =)
@@ -28,7 +30,7 @@ class ServerProtocol(LineOnlyReceiver):
 
         if self.login is not None:
             content = f"Message from {self.login}: {content}"
-
+            self.add_history(content)
             for user in self.factory.clients:
                 if user is not self:
                     user.sendLine(content.encode())
@@ -36,9 +38,26 @@ class ServerProtocol(LineOnlyReceiver):
             # login:admin -> admin
             if content.startswith("login:"):
                 self.login = content.replace("login:", "")
-                self.sendLine("Welcome!".encode())
+                if self.login:
+                    if self.login in self.logins:
+                        # кириллица на windows 10 кракозябры показывает, поэтому...
+                        self.sendLine(f"This login {self.login} used".encode())
+                    else:
+                        self.send_history()
+                        self.logins.append(self.login)
+                        self.sendLine(f"Welcome {self.login}!".encode())
             else:
                 self.sendLine("Invalid login".encode())
+
+    def send_history(self):
+        if self.history_list:
+            for msg in self.history_list:
+                self.sendLine(msg.encode())
+
+    def add_history(self, msg):
+        if len(self.history_list) >= 10:
+            del self.history_list[0]
+        self.history_list.append(msg)
 
 
 class Server(ServerFactory):
